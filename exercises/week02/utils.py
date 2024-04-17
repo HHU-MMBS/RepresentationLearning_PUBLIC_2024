698e39387e08f03fe7aed6f7bedf4f203c5cb8e0
Felix Michels <felixm.michels@gmail.com>
2024-04-17 15:06:21 +0200

    utils update

--- exercises/week02/utils.py
+++ exercises/week02/utils.py
@@ -1,18 +1,10 @@
-
-import os
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import DataLoader, Dataset
-import torchvision
-import torchvision.transforms as transforms
-import torch.optim as optim
-import torch.utils.data as data
-import random
 import matplotlib.pyplot as plt
 from torchvision import transforms as T
-from tqdm import tqdm
+from tqdm.notebook import tqdm
+
 
 def imshow(img, i=0, mean=torch.tensor([0.0], dtype=torch.float32), std=torch.tensor([1], dtype=torch.float32)):
     """
@@ -24,41 +16,38 @@ def imshow(img, i=0, mean=torch.tensor([0.0], dtype=torch.float32), std=torch.te
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
 
+@torch.no_grad()
 def prevalidate(model, val_loader,criterion, device):
-    ### START CODE HERE ### (≈ 12 lines of code)
     model.eval()
-    correct, total = 0, 0
     loss_step = []
-    with torch.no_grad():
-        for data in val_loader:
-            inp_data,labels = data
-            inp_data, labels = inp_data.to(device), labels.to(device)
-            outputs = model(inp_data)
-            val_loss = criterion(outputs, labels)
-            loss_step.append(val_loss.item())
-        # dont forget to take the means here
-        val_loss_epoch = np.mean(loss_step)
-    ### END CODE HERE ###
-        return val_loss_epoch
+    for inp_data, labels in val_loader:
+        inp_data, labels = inp_data.to(device), labels.to(device)
+        outputs = model(inp_data)
+        val_loss = criterion(outputs, labels)
+        loss_step.append(val_loss.item())
+    # dont forget to take the means here
+    val_loss_epoch = np.mean(loss_step)
+    return val_loss_epoch
+
 
 def pretrain_one_epoch(model, optimizer, train_loader, criterion, device):
-    ### START CODE HERE ### (≈ 12 lines of code)
     model.train()
     loss_step = []
-    for data in train_loader:
+    for data in tqdm(train_loader, leave=False, desc='Training'):
         # Move the data to the GPU
         inp_data, labels = data
         inp_data, labels = inp_data.to(device), labels.to(device)
-        outputs = model(inp_data)
-        loss = criterion(outputs, labels)
-        optimizer.zero_grad()
+        with torch.autocast(device_type=device.type, dtype=torch.float16):
+            outputs = model(inp_data)
+            loss = criterion(outputs, labels)
+        optimizer.zero_grad(set_to_none=True)
         loss.backward()
         optimizer.step()
         loss_step.append(loss.item())
     # dont forget the means here
     loss_curr_epoch = np.mean(loss_step)
-    ### END CODE HERE ###
     return loss_curr_epoch
+
 
 def save_model(model, path, epoch, optimizer, val_loss):
     torch.save({
@@ -69,8 +58,8 @@ def save_model(model, path, epoch, optimizer, val_loss):
         }, path)
 
 def pretrain(model, optimizer, num_epochs, train_loader, val_loader, criterion, device):
-    dict_log = {"train_loss":[], "val_loss":[]}
-    ### START CODE HERE ### (≈ 12 lines of code)
+    dict_log = {"train_loss": [], "val_loss": []}
+    device = torch.device(device)
     best_val_loss = 1e8
     model = model.to(device)
     pbar = tqdm(range(num_epochs))
@@ -78,8 +67,8 @@ def pretrain(model, optimizer, num_epochs, train_loader, val_loader, criterion, 
         loss_curr_epoch = pretrain_one_epoch(model, optimizer, train_loader, criterion, device)
         val_loss = prevalidate(model, val_loader, criterion, device)
 
-        # Print epoch results to screen 
-        msg = (f'Ep {epoch}/{num_epochs}: || Loss: Train {loss_curr_epoch:.3f} \t Val {val_loss:.3f}')
+        # Print epoch results to screen
+        msg = f'Loss: Train {loss_curr_epoch:.3f} \t Val {val_loss:.3f}'
         pbar.set_description(msg)
 
         dict_log["train_loss"].append(loss_curr_epoch)
@@ -88,34 +77,34 @@ def pretrain(model, optimizer, num_epochs, train_loader, val_loader, criterion, 
         # Use this code to save the model with the best validation loss
         if val_loss < best_val_loss:
             best_val_loss = val_loss
-            save_model(model, f'best_model_min_val_loss.pth', epoch, optimizer, val_loss)   
-        ### END CODE HERE ### 
+            save_model(model, f'best_model_min_val_loss.pth', epoch, optimizer, val_loss)
     return dict_log
 
 
-
+@torch.no_grad()
 def validate(model, val_loader, device):
     model.eval()
     criterion = nn.CrossEntropyLoss()
     correct, total = 0, 0
     loss_step = []
-    with torch.no_grad():
-        for data in val_loader:
-            inp_data,labels = data
-            inp_data = inp_data.to(device)
-            labels = labels.to(device)
-            outputs = model(inp_data)
-            val_loss = criterion(outputs, labels)
-            predicted = torch.max(outputs, 1)[1]
-            total += labels.size(0)
-            correct += (predicted == labels).sum()
-            loss_step.append(val_loss.item())
-        # dont forget to take the means here
-        val_acc = (100 * correct / total).cpu().numpy() 
-        val_loss_epoch = torch.tensor(loss_step).mean().numpy()
-        return val_acc , val_loss_epoch
+    for data in val_loader:
+        inp_data, labels = data
+        inp_data = inp_data.to(device)
+        labels = labels.to(device)
+        outputs = model(inp_data)
+        val_loss = criterion(outputs, labels)
+        predicted = torch.max(outputs, 1)[1]
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
+        loss_step.append(val_loss.item())
+    # dont forget to take the means here
+    val_acc = (100 * correct / total).cpu().numpy() 
+    val_loss_epoch = torch.tensor(loss_step).mean().numpy()
+    return val_acc, val_loss_epoch
+
 
 # Provided
+@torch.no_grad()
 def get_features(model, dataloader, device):
     model = model.to(device)
     feats, labs = [], []
@@ -139,12 +128,12 @@ def train_one_epoch(model, optimizer, train_loader, device):
     correct, total = 0, 0
     for data in train_loader:
         # Move the data to the GPU
-        inp_data,labels = data
+        inp_data, labels = data
         inp_data = inp_data.to(device)
         labels = labels.to(device)
         outputs = model(inp_data)
         loss = criterion(outputs, labels)
-        optimizer.zero_grad()
+        optimizer.zero_grad(set_to_none=True)
         loss.backward()
         optimizer.step()
         with torch.no_grad():
@@ -162,7 +151,7 @@ def linear_eval(model, optimizer, num_epochs, train_loader, val_loader, device):
     best_val_loss = 1e8
     best_val_acc = 0
     model = model.to(device)
-    dict_log = {"train_acc_epoch":[], "val_acc_epoch":[], "loss_epoch":[], "val_loss":[]}
+    dict_log = {"train_acc_epoch": [], "val_acc_epoch": [], "loss_epoch": [], "val_loss": []}
     train_acc, _ = validate(model, train_loader, device)
     val_acc, _ = validate(model, val_loader, device)
     print(f'Init Accuracy of the model: Train:{train_acc:.3f} \t Val:{val_acc:3f}')
@@ -187,7 +176,7 @@ def linear_eval(model, optimizer, num_epochs, train_loader, val_loader, device):
                   'model_state_dict': model.state_dict(),
                   'optimizer_state_dict': optimizer.state_dict(),
                   'loss': val_loss,
-                  }, f'best_model_min_val_loss.pth')
+                  }, 'best_model_min_val_loss.pth')
         
         if val_acc > best_val_acc:
             best_val_acc = val_acc
@@ -196,7 +185,7 @@ def linear_eval(model, optimizer, num_epochs, train_loader, val_loader, device):
                   'model_state_dict': model.state_dict(),
                   'optimizer_state_dict': optimizer.state_dict(),
                   'loss': val_loss,
-                  }, f'best_model_max_val_acc.pth')
+                  }, 'best_model_max_val_acc.pth')
     return dict_log
 
 
@@ -205,3 +194,4 @@ def load_model(model, path):
     model.load_state_dict(checkpoint['model_state_dict'])
     print(f"Model {path} is loaded from epoch {checkpoint['epoch']} , loss {checkpoint['loss']}")
     return model
+
